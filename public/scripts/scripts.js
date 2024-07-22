84d671bc6a0a203178fd762a0b2e0259270b9c45
Rahul Bhikundia <rahulkumar6222@gmail.com>
2024-07-22 22:29:43 +0530

    minor updates

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -2,3 +2,9 @@ $(".unique").click(function (event) {
   var index = $(this).attr("id");
   $("#confirmDeleteLink").attr("href", "/delete/" + index);
 });
+
+$("#exampleFormControlTextarea1").on("click", function () {
+  $(this).toggleClass("expand");
+});
+
+
